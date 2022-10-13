2d76d4e85a0e906410fd0c5332ce6cd46e14012e
jhonk2000 <jhonk-2000@hotmail.com>
2022-10-13 14:18:58 -0500

    Reto de descuento mas optimizado

--- descuentos.js
+++ descuentos.js
@@ -1,8 +1,17 @@
 //array cupones
 const coupons = [
-   "JuanDC_es_Batman",
-   "pero_no_le_digas_a_nadie",
-   "es_un_secreto",
+   {
+       name: "JuanDC_es_Batman",
+       discount: 15,
+   },
+   {
+       name: "pero_no_le_digas_a_nadie",
+       discount: 30,
+   },
+   {
+       name: "es_un_secreto",
+       discount: 25,
+   },
 ];
 
 
@@ -19,7 +28,7 @@ const coupons = [
    const couponValue = InputCoupon.value;
 
 
-  // /opcion 1
+  // Solución #1: arrays y switch
 //    let descuento;
 
 //   switch(couponValue) {
@@ -32,21 +41,35 @@ const coupons = [
 //     case coupons[2]: // "es_un_secreto"
 //       descuento = 25;
 //     break;
-//opcion 2
-if (!coupons.includes(couponValue)) {
-   alert("El cupón " + couponValue + "no es válido");
-} else if (couponValue === "JuanDC_es_Batman") {
-   descuento = 15;
-} else if (couponValue === "pero_no_le_digas_a_nadie") {
-   descuento = 30;
-} else if (couponValue === "es_un_secreto") {
-   descuento = 25;
-}
-  
-//opcion 
+//Solución #2: legibilidad, error first y muerte al switch
 
-  const precioConDescuento =  calcularPrecioConDescuento(precioValue,descuento);
+// if (!coupons.includes(couponValue)) {
+//    alert("El cupón " + couponValue + "no es válido");
+// } else if (couponValue === "JuanDC_es_Batman") {
+//    descuento = 15;
+// } else if (couponValue === "pero_no_le_digas_a_nadie") {
+//    descuento = 30;
+// } else if (couponValue === "es_un_secreto") {
+//    descuento = 25;
+// }
+  
+//opcion 3 Array de cupones 2.0: ahora también con los descuentos
+
+const isCouponValueValid = function (coupon) {
+   return coupon.name === couponValue;  
+};
+
+const userCoupon = coupons.find(isCouponValueValid);
+
+if (!userCoupon) {
+   alert("El cupón " + couponValue + "no es válido");
+}else {
+   const descuento = userCoupon.discount;
+   const precioConDescuento = calcularPrecioConDescuento(precioValue, descuento);
+
    const resultP = document.getElementById("ResultP");
-   resultP.innerHTML = "El precio con descuento es $" + precioConDescuento;
-   
+   resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
+
+     
  }
